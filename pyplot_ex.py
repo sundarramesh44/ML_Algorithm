861a3a70eb7cd29202d4bfefea155a0d4be892ab
sundarramesh44 <sundarramesh44@gmail.com>
2025-11-15 23:54:23 +0530

    Add files via upload

---
+++ pyplot_ex.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plot
+x = [10,  20,  30, 40, 50, 60]
+y = [100, 200, 300,400,500,600]
+plot.scatter(x, y, color = red)
+plot.title('Trying in ubuntu inside docker')
+plot.xlabel('X axis')
+plot.ylabel('Y axis')
+plot.show()
